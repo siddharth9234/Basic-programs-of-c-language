54888d79e1e7ef794da072550f6e8efc3f57a4db
siddharth9234 <siddharthsirohi011@gmail.com>
2026-05-20 12:50:45 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	float a;
+	float b;
+	float c;
+	printf("We are here to find the area of your rectangle\n");
+	printf("Enter the length of your rectangle:");
+	scanf("%f",&a);
+	printf("Enter the breadth of your rectangle:");
+	scanf("%f",&b);
+
+	c = a*b;
+	printf("AREA OF YOUR RECTANGLE IS: %f",c);
+
+
+
+
+
+}
